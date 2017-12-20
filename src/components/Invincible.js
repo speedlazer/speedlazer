678b9f801595e94c9433f689bdbef34e79248f14
Matthijs Groen <matthijs.groen@gmail.com>
2017-12-20 20:28:30 +0100

    Migrate invincible 💪

---
+++ src/components/Invincible.js
@@ -0,0 +1,24 @@
+Crafty.c("Invincible", {
+  init() {
+    this.requires("Delay");
+    this.blinkOn = true;
+  },
+
+  _blink() {
+    this.blinkOn = !this.blinkOn;
+    this.alpha = this.blinkOn ? 0.5 : 1.0;
+  },
+
+  remove() {
+    this.cancelDelay(this._blink);
+    this.alpha = 1.0;
+  },
+
+  invincibleDuration(duration) {
+    this.delay(this._blink, 250, -1);
+    if (duration !== -1) {
+      this.delay(() => this.removeComponent("Invincible"), duration, 0);
+    }
+    return this;
+  }
+});
