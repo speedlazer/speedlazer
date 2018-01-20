4e595315f3527fce4b15b84b87484e0eba8c354b
Matthijs Groen <matthijs.groen@gmail.com>
2018-01-20 13:37:13 +0100

    Fix issue with game assets

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -17,7 +17,8 @@ module.exports = {
   },
   output: {
     filename: "[name].[hash].js",
-    path: path.resolve(__dirname, "..", "dist")
+    path: path.resolve(__dirname, "..", "dist"),
+    publicPath: "./"
   },
   resolve: {
     alias: {
