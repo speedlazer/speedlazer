12af89944da0e576120ae18ae9fc063d0b6626e1
Matthijs Groen <matthijs.groen@gmail.com>
2018-01-20 17:52:28 +0100

    Some cleanup and extra output

--- .travis/beta-deploy.sh
+++ .travis/beta-deploy.sh
@@ -2,6 +2,9 @@
 set -e
 git config user.name "Travis"
 git config user.email "travis@travis-ci.org"
+echo "Setting remote"
 git remote add origin https://${GITHUB_TOKEN}@github.com/speedlazer/speedlazer.git > /dev/null 2>&1
+echo "Starting build and deploy"
 bin/beta-deploy.sh
+echo "Pushing update to github pages"
 git push origin gh-pages
