12aa96d8e6fc510111a0235d6c18f1d6d1bff2a4
Matthijs Groen <matthijs.groen@gmail.com>
2018-01-20 20:25:24 +0100

    Fix remote branch pulling

--- .travis/beta-deploy.sh
+++ .travis/beta-deploy.sh
@@ -4,7 +4,8 @@ git config user.name "Travis"
 git config user.email "travis@travis-ci.org"
 echo "Setting remote"
 git remote add origin-gh https://${GITHUB_TOKEN}@github.com/speedlazer/speedlazer.git > /dev/null 2>&1
-git checkout origin-gh/gh-pages
+git fetch origin-gh
+git checkout -b gh-pages origin-gh/gh-pages
 git checkout -
 echo "Starting build and deploy"
 bin/beta-deploy.sh
