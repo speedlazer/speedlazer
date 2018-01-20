18ef7ef11a05cbc699ad2e4e7c55ab9ed7baaf68
Matthijs Groen <matthijs.groen@gmail.com>
2018-01-20 19:49:57 +0100

    Rename remote

--- .travis/beta-deploy.sh
+++ .travis/beta-deploy.sh
@@ -3,9 +3,10 @@ set -e
 git config user.name "Travis"
 git config user.email "travis@travis-ci.org"
 echo "Setting remote"
-git remote add origin https://${GITHUB_TOKEN}@github.com/speedlazer/speedlazer.git
-#> /dev/null 2>&1
+git remote add origin-gh https://${GITHUB_TOKEN}@github.com/speedlazer/speedlazer.git > /dev/null 2>&1
+git checkout origin-gh/gh-pages gh-pages
+git checkout -
 echo "Starting build and deploy"
 bin/beta-deploy.sh
 echo "Pushing update to github pages"
-git push origin gh-pages
+git push origin-gh gh-pages
