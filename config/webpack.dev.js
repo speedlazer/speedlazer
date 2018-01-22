bf46d1f719e777c56023eb2adfcf0c910c92d277
Matthijs Groen <matthijs.groen@gmail.com>
2018-01-22 17:27:12 +0100

    Inject version into HTML body

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,12 +1,16 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 const webpack = require("webpack");
+const buildVersion = function() {
+  return require("../package.json").version;
+};
 
 module.exports = merge(common, {
   plugins: [
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
+      "process.env.VERSION": JSON.stringify(`${buildVersion()} DEV`),
       "process.env.GA_TRACKER": JSON.stringify("UA-Tracker")
     })
   ],
