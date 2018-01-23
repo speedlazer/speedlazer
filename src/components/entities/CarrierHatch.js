3d75df4cd95b3afec2ad96ad6237ef5c3b644eb4
Matthijs Groen <matthijs.groen@gmail.com>
2018-01-23 23:16:01 +0100

    Prettier

--- src/components/entities/CarrierHatch.js
+++ src/components/entities/CarrierHatch.js
@@ -1,14 +1,14 @@
 const MOVE_X = -32;
 const MOVE_Y = 16;
 
-Crafty.c('CarrierHatch', {
+Crafty.c("CarrierHatch", {
   init() {
-    this.requires('2D, WebGL, aircraftCarrierOpenHatch');
-    this.attr({z: -10});
-    this.crop(0, 2, 5*32, 32);
+    this.requires("2D, WebGL, aircraftCarrierOpenHatch");
+    this.attr({ z: -10 });
+    this.crop(0, 2, 5 * 32, 32);
 
-    this.lid = Crafty.e('2D, WebGL, aircraftCarrierHatchLid, Tween, Delta2D');
-    this.lid.crop(0, 2, 5*32, 32);
+    this.lid = Crafty.e("2D, WebGL, aircraftCarrierHatchLid, Tween, Delta2D");
+    this.lid.crop(0, 2, 5 * 32, 32);
     this.lid.attr({
       z: -9,
       x: this.x,
@@ -16,8 +16,8 @@ Crafty.c('CarrierHatch', {
     });
     this.attach(this.lid);
 
-    this.dust = Crafty.e('2D, WebGL, Explode, ColorEffects');
-    this.dust.colorOverride('#808080');
+    this.dust = Crafty.e("2D, WebGL, Explode, ColorEffects");
+    this.dust.colorOverride("#808080");
     this.dust.attr({
       w: 150,
       alpha: 0,
@@ -29,17 +29,17 @@ Crafty.c('CarrierHatch', {
   },
 
   open() {
-    this.dust.alpha = .2;
-    this.lid.attr({dy: 0, dx: 0});
+    this.dust.alpha = 0.2;
+    this.lid.attr({ dy: 0, dx: 0 });
     this.lid.tween(
       {
         dx: MOVE_X,
         dy: MOVE_Y
       },
       600,
-      'easeInOutQuad'
+      "easeInOutQuad"
     );
-    this.dust.one('TweenEnd', () => {
+    this.dust.one("TweenEnd", () => {
       this.dust.alpha = 0;
     });
     this.dust.playExplode(800);
@@ -56,15 +56,14 @@ Crafty.c('CarrierHatch', {
         dx: 0
       },
       600,
-      'easeInOutQuad'
+      "easeInOutQuad"
     );
-    this.lid.one('TweenEnd', () => {
-      this.dust.alpha = .2;
-      this.dust.one('AnimationEnd', () => {
+    this.lid.one("TweenEnd", () => {
+      this.dust.alpha = 0.2;
+      this.dust.one("AnimationEnd", () => {
         this.dust.alpha = 0;
       });
       this.dust.playExplode(800);
     });
   }
-
 });
