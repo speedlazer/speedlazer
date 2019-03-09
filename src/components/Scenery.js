69bef8ec332112a78fbc2038eeefffe693476e9e
Matthijs Groen <matthijs@kabisa.nl>
2019-03-09 18:07:19 +0100

    Start rendering scenery

--- src/components/Scenery.js
+++ src/components/Scenery.js
@@ -7,33 +7,30 @@ export const setScenery = sceneryName => {
   scenery.setNextScenery(sceneryName);
 };
 
-Crafty.c("SceneryBlock", {
-  updateSceneryPositions() {
-    console.log("update children");
-    const cameraCenter = {
-      x: Crafty.viewport.width / 2,
-      y: Crafty.viewport.height / 2
-    };
-    const blockCenter = {
-      x: this.x + this.w / 2,
-      y: this.y + this.h / 2
-    };
-    console.log({ cameraCenter, blockCenter });
-
-    this._children.forEach(child => {
-      // 0 = blockCenter - (this.w * child.distance)
-      console.log("distance", child.distance);
-    });
-  }
-});
+Crafty.c("SceneryBlock", {});
 
 const createBlock = (scenery, x, y) => {
-  const block = Crafty.e("2D, WebGL, Color, SceneryBlock")
-    .color("#0000FF")
-    .attr({ x, y, z: -1000, w: scenery.width, h: scenery.height });
+  const block = Crafty.e("2D, WebGL, SceneryBlock").attr({
+    x,
+    y,
+    w: scenery.width,
+    h: scenery.height
+  });
+  const cameraCenter = {
+    x: Crafty.viewport.width / 2,
+    y: Crafty.viewport.height / 2
+  };
+  const halfW = block.w / 2;
+  const halfH = block.h / 2;
+  const blockCenter = {
+    x: x + halfW,
+    y: y + halfH
+  };
   scenery.elements.forEach(element => {
-    const elementX = (element.x < 0 ? scenery.width : 0) + x + element.x;
-    const elementY = (element.y < 0 ? scenery.height : 0) + y + element.y;
+    const distance = element.distance === undefined ? 1 : element.distance;
+    const elementX = (element.x < 0 ? scenery.width * distance : 0) + element.x;
+    const elementY =
+      (element.y < 0 ? scenery.height * distance : 0) + element.y;
 
     let entity;
     if (element.composition) {
@@ -49,14 +46,20 @@ const createBlock = (scenery, x, y) => {
         h: element.h
       });
     }
+    const centerX =
+      blockCenter.x * distance + cameraCenter.x * (1.0 - distance);
+    const centerY =
+      blockCenter.y * distance + cameraCenter.y * (1.0 - distance);
+    const left = centerX - halfW * distance;
+    const top = centerY - halfH * distance;
+
     entity.attr({
-      x: elementX,
-      y: elementY,
-      distance: element.distance || 1
+      x: Math.floor(left + elementX),
+      y: Math.floor(top + elementY),
+      distance
     });
     block.attach(entity);
   });
-  block.updateSceneryPositions();
 
   return block;
 };
@@ -70,7 +73,9 @@ Crafty.c("Scenery", {
   setNextScenery(sceneryName) {
     const scenery = sceneries[sceneryName];
     if (!scenery) return;
-    const block = createBlock(scenery, 0, 0);
-    //Crafty.viewport.scale(0.5);
+
+    createBlock(scenery, 0, 0);
+    createBlock(scenery, 1024, 0);
+    createBlock(scenery, -1024, 0);
   }
 });
