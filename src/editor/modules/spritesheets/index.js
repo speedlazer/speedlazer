a6b64357f4a4ef7c916c820efb317d0edbd896bd
Matthijs Groen <matthijs@kabisa.nl>
2019-10-22 16:24:00 +0200

    Add support for clicking on sprites

--- src/editor/modules/spritesheets/index.js
+++ src/editor/modules/spritesheets/index.js
@@ -7,6 +7,7 @@ import { ScrollBox } from "../../components/ScrollBox";
 import { Highlight } from "./components/Highlight";
 import { Title } from "../../components/Title";
 import spritesheets from "src/data/spritesheets";
+import { route } from "preact-router";
 
 class Spritesheets extends Component {
   constructor(props) {
@@ -16,6 +17,24 @@ class Spritesheets extends Component {
       height: null
     };
   }
+
+  onLayerClick = event => {
+    const activeMap =
+      spritesheets.find(m => m.name === this.props.map) || spritesheets[0];
+    const tileWidth = activeMap.map.tile;
+    const tileHeight = activeMap.map.tileh;
+
+    const tileX = event.offsetX / tileWidth;
+    const tileY = event.offsetY / tileHeight;
+
+    const sprite = Object.entries(activeMap.map.map).find(
+      ([, [x, y, w, h]]) =>
+        tileX > x && tileX < x + w && tileY > y && tileY < y + h
+    );
+    if (sprite) {
+      route(`/editor/sprites/${activeMap.name}/${sprite[0]}`, true);
+    }
+  };
 
   render({ map, activeSprite }, { width, height }) {
     const activeMap = spritesheets.find(m => m.name === map) || spritesheets[0];
@@ -28,11 +47,14 @@ class Spritesheets extends Component {
         <Title>
           Spritesheets - {activeMap.name} {width}x{height}
         </Title>
-        <Menu
-          items={spritesheets.map(map => [map.name, `/sprites/${map.name}`])}
-        />
         <Divider>
           <div>
+            <Menu
+              items={spritesheets.map(map => [
+                map.name,
+                `/sprites/${map.name}`
+              ])}
+            />
             <Menu
               items={Object.keys(activeMap.map.map).map(key => [
                 key,
@@ -40,33 +62,39 @@ class Spritesheets extends Component {
               ])}
             />
           </div>
-          <ScrollBox height={"100%"} width={"80vw"}>
+          <div>
             {highlight && (
               <p style={{ color: "white" }}>
                 {activeSprite} - {tileWidth * highlight[2]} x{" "}
                 {tileHeight * highlight[3]}
               </p>
             )}
-            <LayerBox width={width} height={height}>
-              <img
-                src={activeMap.image}
-                onLoad={e => {
-                  this.setState({
-                    width: e.target.naturalWidth,
-                    height: e.target.naturalHeight
-                  });
-                }}
-              />
-              {highlight && (
-                <Highlight
-                  highlight={highlight}
-                  tileWidth={tileWidth}
-                  tileHeight={tileHeight}
+            <ScrollBox height={"80vh"} width={"80vw"}>
+              <LayerBox
+                width={width}
+                height={height}
+                onClick={this.onLayerClick}
+              >
+                <img
+                  src={activeMap.image}
+                  onLoad={e => {
+                    this.setState({
+                      width: e.target.naturalWidth,
+                      height: e.target.naturalHeight
+                    });
+                  }}
                 />
-              )}
-              <Grid width={tileWidth} height={tileHeight} />
-            </LayerBox>
-          </ScrollBox>
+                {highlight && (
+                  <Highlight
+                    highlight={highlight}
+                    tileWidth={tileWidth}
+                    tileHeight={tileHeight}
+                  />
+                )}
+                <Grid width={tileWidth} height={tileHeight} />
+              </LayerBox>
+            </ScrollBox>
+          </div>
         </Divider>
       </section>
     );
