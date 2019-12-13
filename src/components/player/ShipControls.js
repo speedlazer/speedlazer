a39e1e25b568ef1373d9b491b1a55b42e7024945
Matthijs Groen <matthijs.groen@gmail.com>
2019-12-13 12:59:38 +0100

    Add reverse flying

--- src/components/player/ShipControls.js
+++ src/components/player/ShipControls.js
@@ -1,6 +1,16 @@
 const ShipControls = "ShipControls";
 
 Crafty.c(ShipControls, {
+  init() {
+    this.bind("NewDirection", direction => {
+      if (direction.x === -1) {
+        this.showState("reverse");
+      } else {
+        this.showState("flying");
+      }
+    });
+  },
+
   controlPrimary(onOff) {
     onOff ? this.showState("shooting") : this.showState("noShooting");
   },
--- src/data/particles/index.js
+++ src/data/particles/index.js
@@ -22,20 +22,43 @@ export default {
     emitter: {
       w: 2,
       h: 2,
-      amount: 100
+      amount: 200
     },
-    gravity: [-50, 0],
+    gravity: [-150, 0],
     particle: {
       sprite: "explosion7",
       angle: 180,
       angleRandom: 0,
       duration: 500,
       durationRandom: 0,
-      velocity: 350,
-      velocityRandom: 0,
+      velocity: 200,
+      velocityRandom: 10,
       startSize: 18,
       endSize: 1,
-      startColor: [0.9, 0.5, 0.5, 0.2],
+      startColor: [0.9, 0.5, 0.5, 0.6],
+      startColorRandom: [0, 0, 0, 0],
+      endColor: [0.9, 0.2, 0.2, 0],
+      endColorRandom: [0, 0, 0, 0]
+    }
+  },
+  "ship.reverse": {
+    emitter: {
+      w: 2,
+      h: 2,
+      amount: 200
+    },
+    gravity: [150, 0],
+    particle: {
+      sprite: "explosion7",
+      angle: 0,
+      angleRandom: 0,
+      duration: 500,
+      durationRandom: 0,
+      velocity: 200,
+      velocityRandom: 10,
+      startSize: 5,
+      endSize: 1,
+      startColor: [0.9, 0.5, 0.5, 0.6],
       startColorRandom: [0, 0, 0, 0],
       endColor: [0.9, 0.2, 0.2, 0],
       endColorRandom: [0, 0, 0, 0]
