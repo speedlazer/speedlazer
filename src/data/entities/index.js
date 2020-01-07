4470be8c5aff20f5f07c5f23441e582cefbe0490
Matthijs Groen <matthijs.groen@gmail.com>
2020-01-07 18:52:29 +0100

    Introship

--- src/data/entities/index.js
+++ src/data/entities/index.js
@@ -1,7 +1,8 @@
 import sun from "./Sun.entity.json";
 import battleShip from "./BattleShip.entity.json";
+import introShip from "./IntroShip.entity.json";
 import playerShip from "./PlayerShip.entity.json";
 import drone from "./Drone.entity.json";
 import weapons from "./Weapons.entity.json";
 
-export default [playerShip, battleShip, drone, sun, weapons];
+export default [playerShip, battleShip, introShip, drone, sun, weapons];
