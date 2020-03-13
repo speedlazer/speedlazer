05abd2ed9fdb9e621f3eebc49e28dd1e63f70803
Matthijs Groen <matthijs.groen@gmail.com>
2020-03-13 20:54:19 +0100

    Add version to production build

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,9 +1,18 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
+const webpack = require("webpack");
 const TerserPlugin = require("terser-webpack-plugin");
+const buildVersion = function() {
+  return require("../package.json").version;
+};
 
 module.exports = merge(common, {
   mode: "production",
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.VERSION": JSON.stringify(`${buildVersion()}`)
+    })
+  ],
   devtool: "source-map",
   optimization: {
     minimize: true,
