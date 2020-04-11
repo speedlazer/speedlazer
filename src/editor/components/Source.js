738414847a15e88a03026dde9925b40db79d0926
Matthijs Groen <matthijs.groen@gmail.com>
2020-04-12 00:27:14 +0200

    Create some syntax highlighting

--- src/editor/components/Source.js
+++ src/editor/components/Source.js
@@ -1,5 +1,66 @@
 import { h, Component } from "preact";
-import { sourceCode, toggleSource } from "./Source.scss";
+import {
+  sourceCode,
+  toggleSource,
+  jsonString,
+  jsonNull,
+  jsonKey,
+  jsonBool,
+  jsonNumber,
+  indent
+} from "./Source.scss";
+
+const replacer = (key, value) => {
+  if (typeof value === "string" && key === value) {
+    return <span class={jsonKey}>&quot;{value}&quot;</span>;
+  }
+  if (typeof value === "string") {
+    return <span class={jsonString}>&quot;{value}&quot;</span>;
+  }
+  if (typeof value === "number") {
+    return <span class={jsonNumber}>{value}</span>;
+  }
+  if (value === null) {
+    return <span class={jsonNull}>null</span>;
+  }
+  if (value === true || value === false) {
+    return <span class={jsonBool}>{value}</span>;
+  }
+  if (Array.isArray(value)) {
+    return (
+      <span>
+        [
+        <div class={indent}>
+          {value.map((v, k, l) => (
+            <span key={k}>
+              {replacer(k, v)}
+              {k < l.length - 1 ? ",\n" : "\n"}
+            </span>
+          ))}
+        </div>
+        ]
+      </span>
+    );
+  }
+  if (typeof value === "object") {
+    return (
+      <span>
+        {"{\n"}
+        <div class={indent}>
+          {Object.entries(value).map(([k, v], i, l) => (
+            <span key={i}>
+              {replacer(k, k)}: {replacer(k, v)}
+              {k < l.length - 1 ? ",\n" : "\n"}
+            </span>
+          ))}
+        </div>
+        {"}"}
+      </span>
+    );
+  }
+
+  return value;
+};
 
 export class Source extends Component {
   state = { collapsed: true };
@@ -16,9 +77,7 @@ export class Source extends Component {
         <a class={toggleSource} href="#" onClick={this.toggleSource}>
           {collapsed ? "Show" : "Hide"} source
         </a>
-        {!collapsed && (
-          <div class={sourceCode}>{JSON.stringify(code, null, 2)}</div>
-        )}
+        {!collapsed && <div class={sourceCode}>{replacer("", code)}</div>}
       </div>
     );
   }
