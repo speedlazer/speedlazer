56fdf9a92fff857c560d94da805f33ee8e9d8cce
Matthijs Groen <matthijs.groen@gmail.com>
2020-04-15 08:13:03 +0200

    Place all content in single string

--- config/loaders/data.js
+++ config/loaders/data.js
@@ -2,12 +2,30 @@
 const fs = require("fs");
 require("@babel/register")({});
 
+const assignUnit = (amount, unitLevel = 0) =>
+  amount > 1024
+    ? assignUnit(amount / 1024, unitLevel + 1)
+    : { amount, unitLevel };
+
+const toHuman = amount => {
+  const withUnit = assignUnit(amount);
+  const scale = ["b", "Kb", "Mb", "Gb"];
+
+  return `${Math.round(withUnit.amount * 100) / 100} ${
+    scale[withUnit.unitLevel]
+  }`;
+};
+
+// Based on:
+// https://v8.dev/blog/cost-of-javascript-2019#json
+
 module.exports = function(input) {
   this.cacheable();
 
   const folders = JSON.parse(input);
   const path = this.resourcePath.split("/").slice(0, -1);
-  const content = folders.map(folder => {
+
+  const allData = folders.reduce((acc, folder) => {
     const dirPath = path.concat(folder);
     const entries = fs.readdirSync(dirPath.join("/"));
 
@@ -33,15 +51,21 @@ module.exports = function(input) {
 
       return acc;
     }, {});
+    return { ...acc, [folder]: struct };
+  }, {});
+  const stringData = JSON.stringify(allData);
 
-    return `
-export const ${folder}Data = JSON.parse("${JSON.stringify(struct).replace(
-      /"/g,
-      '\\"'
-    )}");
-export const ${folder} = name => ${folder}Data[name];
+  console.log(`Bundling ${toHuman(stringData.length)} of game data`);
+  const content = `
+const data = JSON.parse("${stringData.replace(/"/g, '\\"')}");
 `;
-  });
+  const exports = folders.map(
+    folder =>
+      `
+export const ${folder}Data = data.${folder};
+export const ${folder} = name => ${folder}Data[name];
+`
+  );
 
-  return content.join("\n\n");
+  return [content].concat(exports).join("\n\n");
 };
