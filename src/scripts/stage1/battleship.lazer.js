246ed29f3ca40ec1d65120fe2526ca7c5684ab1a
Matthijs Groen <matthijs.groen@gmail.com>
2020-04-20 23:11:38 +0200

    Fix linting error

--- src/scripts/stage1/battleship.lazer.js
+++ src/scripts/stage1/battleship.lazer.js
@@ -28,7 +28,7 @@ const part = async ({
   text.remove();
   exec(playerShip({ existing: true }));
 
-  //await exec(droneWave(5, "drone.pattern2", 500));
+  await exec(droneWave(5, "drone.pattern2", 500));
   //await exec(droneWave(5, "drone.pattern1", 500));
   //await exec(droneWave(5, "drone.pattern1", 500));
 
