67375a2b809d752edd52610a2d2f63bd14e1f948
Matthijs Groen <matthijs.groen@gmail.com>
2020-04-20 22:23:49 +0200

    Add ending

---
+++ src/scripts/end.lazer.js
@@ -0,0 +1,9 @@
+import { bigText } from "src/components/BigText";
+
+const part = async () => {
+  const text = bigText("Congratulations!", { sup: "The end for now..." });
+  await text.fadeIn(5000);
+  text.remove();
+};
+
+export default part;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,13 @@
 import s1p1 from "./stage1/intro.lazer";
 import s1p2 from "./stage1/hacked.lazer";
 import s1p3 from "./stage1/battleship.lazer";
+import end from "./end.lazer";
 
 const gameStructure = [
   { name: "stage1.intro", script: s1p1 },
   { name: "stage1.hacked", script: s1p2 },
-  { name: "stage1.battleship", script: s1p3, wip: true }
+  { name: "stage1.battleship", script: s1p3, wip: true },
+  { name: "end", script: end }
 ];
 
 export default gameStructure;
